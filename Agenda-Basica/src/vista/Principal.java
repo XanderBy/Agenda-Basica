545e8756630090a130ef3908507c736a651ab365
XanderBy <33516014+XanderBy@users.noreply.github.com>
2018-05-17 23:52:38 +0200

    Añadidas Algunas Interfaces

--- Agenda-Basica/src/vista/Principal.java
+++ Agenda-Basica/src/vista/Principal.java
@@ -33,13 +33,13 @@ public class Principal extends javax.swing.JFrame {
         PantallaAnadirContactoNombre = new javax.swing.JTextField();
         PantallaAnadirContactoTelefono = new javax.swing.JTextField();
         PantallaAnadirContactoBotonAnadir = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        PrincipalListadoContactos = new javax.swing.JList<>();
         PrincipalBotonAnadirContacto = new javax.swing.JButton();
         PrincipalBotonAnadirGrupo = new javax.swing.JButton();
         PrincipalBotonContactos = new javax.swing.JButton();
         PrincipalBotonGrupos = new javax.swing.JButton();
         PrincipalBotonFavoritos = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         jLabel1.setText("nombre");
 
@@ -84,13 +84,6 @@ public class Principal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        PrincipalListadoContactos.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(PrincipalListadoContactos);
-
         PrincipalBotonAnadirContacto.setText("Añadir Contacto");
 
         PrincipalBotonAnadirGrupo.setText("Añadir Grupo");
@@ -101,13 +94,26 @@ public class Principal extends javax.swing.JFrame {
 
         PrincipalBotonFavoritos.setText("favoritos");
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(46, 46, 46)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(45, 45, 45)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(PrincipalBotonAnadirGrupo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -120,7 +126,7 @@ public class Principal extends javax.swing.JFrame {
                 .addComponent(PrincipalBotonGrupos)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(PrincipalBotonFavoritos)
-                .addContainerGap(97, Short.MAX_VALUE))
+                .addContainerGap(189, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -132,14 +138,15 @@ public class Principal extends javax.swing.JFrame {
                             .addComponent(PrincipalBotonContactos)
                             .addComponent(PrincipalBotonGrupos)
                             .addComponent(PrincipalBotonFavoritos))
-                        .addGap(35, 35, 35)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(79, 79, 79)
                         .addComponent(PrincipalBotonAnadirContacto, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(PrincipalBotonAnadirGrupo, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(57, Short.MAX_VALUE))
+                        .addComponent(PrincipalBotonAnadirGrupo, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
@@ -190,9 +197,9 @@ public class Principal extends javax.swing.JFrame {
     public javax.swing.JButton PrincipalBotonContactos;
     public javax.swing.JButton PrincipalBotonFavoritos;
     public javax.swing.JButton PrincipalBotonGrupos;
-    public javax.swing.JList<String> PrincipalListadoContactos;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    public javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }

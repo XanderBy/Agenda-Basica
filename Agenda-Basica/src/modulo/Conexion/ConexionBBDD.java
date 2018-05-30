30eeccf66dbe0caed996629172a6c848fcb5901d
Alex <33516014+XanderBy@users.noreply.github.com>
2018-05-31 01:49:43 +0200

    E añadido la clase conexion con la bbdd

--- Agenda-Basica/src/modulo/Conexion/ConexionBBDD.java
+++ Agenda-Basica/src/modulo/Conexion/ConexionBBDD.java
@@ -5,10 +5,43 @@
  */
 package modulo.Conexion;
 
-/**
- *
- * @author alexi
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBBDD {
-    
+	// Librería de MySQL
+	public String driver = "com.mysql.jdbc.Driver";
+
+	// Nombre de la base de datos
+	public String database = "Agenda";
+
+	// Host
+	public String hostname = "localhost";
+
+	// Puerto
+	public String port = "3306";
+
+	// Ruta de nuestra base de datos (desactivamos el uso de SSL con
+	// "?useSSL=false")
+	public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false";
+
+	// Nombre de usuario
+	public String username = "root";
+
+	// Clave de usuario
+	public String password = "";
+
+	public Connection conectarMySQL() {
+		Connection conn = null;
+
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
 }

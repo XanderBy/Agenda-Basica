368ccf14afa51de581934c3d2c29f77d713fe628
Alex <33516014+XanderBy@users.noreply.github.com>
2018-05-31 01:54:46 +0200

    Eliminado lo que sobraba

--- Agenda-Basica/src/modulo/metodos/MetodosContactos.java
+++ Agenda-Basica/src/modulo/metodos/MetodosContactos.java
@@ -1,34 +1,27 @@
 package modulo.metodos;
 
-import java.util.HashMap;
-
-import modulo.POJOs.Contactos;
 
 public class MetodosContactos {
-	public static HashMap <Integer, Contactos>ContactosGrupo=new HashMap <Integer, Contactos>();
 
 	public void AnadirContacto(int numero, String nombre) {
-		Contactos contacto=new Contactos(nombre ,numero);
-		ContactosGrupo.put(numero, contacto);
+		
 		
 	}
 
 
 	public void EliminarContacto(int numero) {
-		ContactosGrupo.remove(numero);
+		
 
 		
 	}
 	
 	public void modificarContacto(String nombre, int numero, int numeroAntiguo) {
-		ContactosGrupo.remove(numeroAntiguo);
-		Contactos contacto=new Contactos(nombre ,numero);
-		ContactosGrupo.put(numero, contacto);
+		
 	}
 	public void anadirFavoritos(int numero) {
-		Contactos.Favoritos.put(numero, ContactosGrupo.get(numero));
+		
 	}
 	public void eliminarFavoritos(int numero) {
-		Contactos.Favoritos.remove(numero);
+		
 	}
 }
--- Agenda-Basica/src/modulo/metodos/MetodosGrupo.java
+++ Agenda-Basica/src/modulo/metodos/MetodosGrupo.java
@@ -1,34 +1,28 @@
 package modulo.metodos;
 
-import java.util.HashMap;
-import modulo.POJOs.Grupos;
 
 public class MetodosGrupo {
-	public static HashMap<String, Grupos> TodosLosGrupos = new HashMap<String, Grupos>();
+
 
 	public void anadirGrupo(String nombre) {
-		Grupos grupo = new Grupos(nombre);
-		TodosLosGrupos.put(nombre, grupo);
+
 	}
 
 	public void eliminarGrupo(String nombre) {
-		TodosLosGrupos.remove(nombre);
+
 	}
 
 	public void modificarGrupo(String nombre, String nombreAntiguo) {
-		TodosLosGrupos.remove(nombreAntiguo);
-		Grupos grupo = new Grupos(nombre);
-		TodosLosGrupos.put(nombre, grupo);
+
 	}
 
 	// Añadir Contactos a grupos
 	public void AnadirContacto(int numero, String nombre, String nombreGrupo) {
-		TodosLosGrupos.get(nombreGrupo).Participantes.put(numero, Grupos.Participantes.get(nombre));
 
 	}
 
 	// Eliminar Contactos a grupos
 	public void EliminarContacto(int numero, String nombreGrupo) {
-		TodosLosGrupos.get(nombreGrupo).Participantes.remove(numero);
+
 	}
 }
